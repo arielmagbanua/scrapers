06fd2cce8c4d958ae4dc060658a4826af639b31c
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2021-07-13 21:07:19 +0800

    Added documentation.

--- src/lawphil/use-cases/getPageContent.ts
+++ src/lawphil/use-cases/getPageContent.ts
@@ -1,5 +1,11 @@
 import LawPhil from '../index';
 
+/**
+ * The use case for getting the page content.
+ *
+ * @param {string} url - The url of the page to scrape.
+ * @param {string | undefined} savePath - The directory path to optionally save the pdf file.
+ */
 export default async (
   url: string,
   savePath: string | undefined = undefined
